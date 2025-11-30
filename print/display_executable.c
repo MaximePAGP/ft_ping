04601302c356c5990371fbf1ad991b0ff566e5fb
Maxime GP <mgrondinp@gmail.com>
2025-11-30 15:55:57 +0100

    i just read the subject :)

--- print/display_executable.c
+++ print/display_executable.c
@@ -6,7 +6,7 @@
 /*   By: magrondi <magrondi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 15:30:22 by magrondi          #+#    #+#             */
-/*   Updated: 2025/11/30 15:32:26 by magrondi         ###   ########.fr       */
+/*   Updated: 2025/11/30 15:55:23 by magrondi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,8 @@
 
 void	display_executable(void)
 {
-	char const	msg[] = "ping: missing host operand \nft_ping "
-		"ft_ping "
-		"\033[31m<IPv4> | <HOSTNAME>\033[0m "
-		"\033[33m[OPTIONS]\033[0m\n"
-		"\n"
-		"\033[31m<IPv4> | <HOSTNAME>\033[0m   : Ip address or DNS (required)\n"
-		"\033[33m[-v] | [-?]\033[0m           \
-			: Display packets errors (optionnal)\n";
+	char const	msg[] = "ft_ping: missing host operand \n"
+		"Try 'ping --help' or 'ping --usage' for more information.\n";
 
 	write(STDERR_FILENO, &msg, strlen(msg));
 }
