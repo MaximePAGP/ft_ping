a1001109e9477d8b092c583a3130b80b8c3349d7
Maxime GP <mgrondinp@gmail.com>
2025-12-21 01:48:09 +0100

    fix misspelling

--- inc/icmp.h
+++ inc/icmp.h
@@ -1,17 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   imcp.h                                             :+:      :+:    :+:   */
+/*   icmp.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: magrondi <magrondi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 13:36:43 by magrondi          #+#    #+#             */
-/*   Updated: 2025/12/18 22:20:08 by magrondi         ###   ########.fr       */
+/*   Updated: 2025/12/21 01:40:34 by magrondi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ICMP_H
 # define ICMP_H
+
+# ifndef ECHO_REQUEST
+#  define ECHO_REQUEST 8
+# endif
+
+# ifndef ECHO_REPLY
+#  define ECHO_REPLY 0
+# endif
+
+# ifndef UNREACHABLE
+#  define UNREACHABLE 3
+# endif
 
 typedef struct s_icmp
 {
@@ -20,7 +32,7 @@ typedef struct s_icmp
 	uint16_t	checksum;
 	uint16_t	id;
 	uint16_t	sequence;
-	uint32_t	rest;
+	uint8_t		payload[56];
 }	t_icmp;
 
 #endif
